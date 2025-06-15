71aada5280dc9d7697c5285afc4e2edd89417713
noushin shahin nia <noushin@noushins-MacBook-Air.local>
2025-06-15 18:11:26 +0330

    addPrivateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard'
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+
+  
+      <Route path="/dashboard" element={<PrivateRoute>  <Dashboard /> </PrivateRoute>  } />
     </Routes>
   )
 }
---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
+function PrivateRoute({children}){
+    const user = useSelector((state) => state.auth.user);
+
+
+    return user ? children : <Navigate to="/" replace />;
+
+}
+
+export default PrivateRoute;
