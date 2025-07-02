1b9a883d9b7d7de2550b1bccd756843fb255fbcf
noushin shahin nia <noushin@noushins-MacBook-Air.local>
2025-07-02 16:08:42 +0330

    add dana font globally

--- src/them.js
+++ src/them.js
@@ -2,7 +2,7 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: {
-    fontFamily: "Yekan, sans-serif",
+    fontFamily: "Dana, sans-serif",
   },
 });
 
